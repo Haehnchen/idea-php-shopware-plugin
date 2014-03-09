b752df0324255620df687e9d764d551f64299744
Daniel Espendiller <espendiller@gmx.de>
2014-03-09 12:34:08 +0100

    add action template resolve

--- src/de/espend/idea/shopware/ShopwarePluginIcons.java
+++ src/de/espend/idea/shopware/ShopwarePluginIcons.java
@@ -5,5 +5,6 @@ import javax.swing.*;
 
 public class ShopwarePluginIcons {
     public static final Icon SHOPWARE = IconLoader.getIcon("icons/shopware.png");
+    public static final Icon SHOPWARE_LINEMARKER = IconLoader.getIcon("icons/shopware_linemarker.png");
     public static final Icon SHOPWARE_SMARTY = IconLoader.getIcon("icons/smarty_shopware.png");
 }
