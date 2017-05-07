036d80beb3a70ac4b5a06914fb7a0b84e49f16a7
Daniel Espendiller <espendiller@gmx.de>
2017-05-07 17:08:50 +0200

    plugin PhpTypeProvider3 migration #45

--- src/de/espend/idea/shopware/types/ShopwareApiResourcesTypeProvider.java
+++ src/de/espend/idea/shopware/types/ShopwareApiResourcesTypeProvider.java
@@ -1,6 +1,5 @@
 package de.espend.idea.shopware.types;
 
-import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.jetbrains.php.PhpIndex;
@@ -8,7 +7,8 @@ import com.jetbrains.php.lang.psi.elements.Method;
 import com.jetbrains.php.lang.psi.elements.MethodReference;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
 import com.jetbrains.php.lang.psi.elements.PhpNamedElement;
-import com.jetbrains.php.lang.psi.resolve.types.PhpTypeProvider2;
+import com.jetbrains.php.lang.psi.resolve.types.PhpType;
+import com.jetbrains.php.lang.psi.resolve.types.PhpTypeProvider3;
 import de.espend.idea.shopware.util.ShopwareUtil;
 import fr.adrienbrault.idea.symfony2plugin.Settings;
 import fr.adrienbrault.idea.symfony2plugin.Symfony2InterfacesUtil;
@@ -16,14 +16,14 @@ import fr.adrienbrault.idea.symfony2plugin.util.PhpElementsUtil;
 import fr.adrienbrault.idea.symfony2plugin.util.PhpTypeProviderUtil;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Set;
 
 /**
  * @author Daniel Espendiller <daniel@espendiller.net>
  */
-public class ShopwareApiResourcesTypeProvider implements PhpTypeProvider2 {
+public class ShopwareApiResourcesTypeProvider implements PhpTypeProvider3 {
 
     final static char TRIM_KEY = '\u0195';
 
@@ -34,9 +34,8 @@ public class ShopwareApiResourcesTypeProvider implements PhpTypeProvider2 {
 
     @Nullable
     @Override
-    public String getType(PsiElement e) {
-
-        if (DumbService.getInstance(e.getProject()).isDumb() || !Settings.getInstance(e.getProject()).pluginEnabled || !Settings.getInstance(e.getProject()).symfonyContainerTypeProvider) {
+    public PhpType getType(PsiElement e) {
+        if (!Settings.getInstance(e.getProject()).pluginEnabled || !Settings.getInstance(e.getProject()).symfonyContainerTypeProvider) {
             return null;
         }
 
@@ -45,11 +44,12 @@ public class ShopwareApiResourcesTypeProvider implements PhpTypeProvider2 {
             return null;
         }
 
-        return PhpTypeProviderUtil.getReferenceSignature((MethodReference) e, TRIM_KEY);
+        String signature = PhpTypeProviderUtil.getReferenceSignatureByFirstParameter((MethodReference) e, TRIM_KEY);
+        return signature == null ? null : new PhpType().add("#" + this.getKey() + signature);
     }
 
     @Override
-    public Collection<? extends PhpNamedElement> getBySignature(String expression, Project project) {
+    public Collection<? extends PhpNamedElement> getBySignature(String expression, Set<String> visited, int depth, Project project) {
 
         // get back our original call
         // since phpstorm 7.1.2 we need to validate this
@@ -71,25 +71,22 @@ public class ShopwareApiResourcesTypeProvider implements PhpTypeProvider2 {
         // get first matched item
         PhpNamedElement phpNamedElement = phpNamedElementCollections.iterator().next();
         if(!(phpNamedElement instanceof Method)) {
-            return phpNamedElementCollections;
+            return Collections.emptySet();
         }
 
         parameter = PhpTypeProviderUtil.getResolvedParameter(phpIndex, parameter);
         if(parameter == null) {
-            return phpNamedElementCollections;
+            return Collections.emptySet();
         }
 
         // finally search the classes
         if(new Symfony2InterfacesUtil().isCallTo((Method) phpNamedElement, "\\Shopware\\Components\\Api\\Manager", "getResource")) {
-
             PhpClass phpClass = ShopwareUtil.getResourceClass(project, parameter);
             if(phpClass != null) {
-                return Arrays.asList(phpClass);
+                return Collections.singletonList(phpClass);
             }
-
         }
 
         return phpNamedElementCollections;
     }
-
 }
