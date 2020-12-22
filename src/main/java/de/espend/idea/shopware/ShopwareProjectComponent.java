499e70f233eb7b29d1713800113d24e766b55ce2
Daniel Espendiller <espendiller@gmx.de>
2020-12-22 11:50:58 +0100

    Fix "IndexNotReadyException" on background event indexing and provide a visible background task in task bar when running

--- src/main/java/de/espend/idea/shopware/ShopwareProjectComponent.java
+++ src/main/java/de/espend/idea/shopware/ShopwareProjectComponent.java
@@ -2,13 +2,17 @@ package de.espend.idea.shopware;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupManager;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.util.indexing.FileBasedIndexImpl;
+import com.intellij.util.indexing.FileBasedIndex;
 import com.jetbrains.php.lang.PhpFileType;
 import de.espend.idea.shopware.index.EventConfigGoToIndex;
 import de.espend.idea.shopware.util.HookSubscriberUtil;
@@ -42,52 +46,10 @@ public class ShopwareProjectComponent implements ProjectComponent {
             return;
         }
 
-        DumbService.getInstance(this.project).smartInvokeLater(() -> {
-
-            if(PhpElementsUtil.getClassInterface(project, "\\Enlight_Controller_Action") == null) {
-                return;
-            }
-
+        StartupManager.getInstance(project).runWhenProjectIsInitialized(() -> {
             timer = new Timer();
-
-            timer.schedule(new TimerTask() {
-                public void run() {
-
-                    if (DumbService.getInstance(project).isDumb()) {
-                        return;
-                    }
-
-                    final Map<String, Collection<String>> events = new HashMap<>();
-                    final Set<String> configs = new HashSet<>();
-
-                    final Collection<VirtualFile> containingFiles = new HashSet<>();
-
-                    ApplicationManager.getApplication().runReadAction(() -> {
-                        if (DumbService.getInstance(project).isDumb()) {
-                            return;
-                        }
-
-                        for(String methodName : EventConfigGoToIndex.METHOD_NAMES) {
-                            FileBasedIndexImpl.getInstance().getFilesWithKey(EventConfigGoToIndex.KEY, new HashSet<>(Collections.singletonList(methodName)), virtualFile -> {
-                                containingFiles.add(virtualFile);
-                                return true;
-                            }, GlobalSearchScope.getScopeRestrictedByFileTypes(GlobalSearchScope.allScope(project), PhpFileType.INSTANCE));
-                        }
-                    });
-
-                    for (VirtualFile virtualFile : containingFiles) {
-                        ApplicationManager.getApplication().runReadAction(new PsiParameterStorageRunnable(project, virtualFile, events, configs));
-                    }
-
-                    HookSubscriberUtil.NOTIFY_EVENTS_MAP.clear();
-                    HookSubscriberUtil.NOTIFY_EVENTS_MAP.putAll(events);
-
-                    ShopwareUtil.PLUGIN_CONFIGS.clear();
-                    ShopwareUtil.PLUGIN_CONFIGS.addAll(configs);
-                }
-            }, 0, DUMPER_PERIODE);
+            timer.schedule(new MyTimerTask(), 0, DUMPER_PERIODE);
         });
-
     }
 
     @Override
@@ -134,5 +96,50 @@ public class ShopwareProjectComponent implements ProjectComponent {
     @Override
     public String getComponentName() {
         return "Shopware Plugin";
+    }
+
+    private class MyTimerTask extends TimerTask {
+        public void run() {
+            DumbService.getInstance(project).smartInvokeLater(() -> {
+                if (DumbService.getInstance(project).isDumb()) {
+                    return;
+                }
+
+                if (PhpElementsUtil.getClassInterface(project, "\\Enlight_Controller_Action") == null) {
+                    return;
+                }
+
+                ProgressManager.getInstance().run(new Task.Backgroundable(project, "Shopware: Event indexer", false) {
+                    @Override
+                    public void run(@NotNull ProgressIndicator progressIndicator) {
+                        final Map<String, Collection<String>> events = new HashMap<>();
+                        final Set<String> configs = new HashSet<>();
+
+                        final Collection<VirtualFile> containingFiles = new HashSet<>();
+
+                        for (String methodName : EventConfigGoToIndex.METHOD_NAMES) {
+                            ApplicationManager.getApplication().runReadAction(() -> {
+                                FileBasedIndex.getInstance().getFilesWithKey(EventConfigGoToIndex.KEY, new HashSet<>(Collections.singletonList(methodName)), virtualFile -> {
+                                    containingFiles.add(virtualFile);
+                                    return true;
+                                }, GlobalSearchScope.getScopeRestrictedByFileTypes(GlobalSearchScope.allScope(project), PhpFileType.INSTANCE));
+                            });
+                        }
+
+                        DumbService.getInstance(project).smartInvokeLater(() -> {
+                            for (VirtualFile virtualFile : containingFiles) {
+                                ApplicationManager.getApplication().runReadAction(new PsiParameterStorageRunnable(project, virtualFile, events, configs));
+                            }
+
+                            HookSubscriberUtil.NOTIFY_EVENTS_MAP.clear();
+                            HookSubscriberUtil.NOTIFY_EVENTS_MAP.putAll(events);
+
+                            ShopwareUtil.PLUGIN_CONFIGS.clear();
+                            ShopwareUtil.PLUGIN_CONFIGS.addAll(configs);
+                        });
+                    }
+                });
+            });
+        }
     }
 }
